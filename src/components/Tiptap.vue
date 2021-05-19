4ff186833ad8df867f18a3ceb8dfd05a2f176727
Bart Louwers <bartenra@gmail.com>
2021-05-19 09:55:00 +0200

    first commit

---
+++ src/components/Tiptap.vue
@@ -0,0 +1,34 @@
+<template>
+  <editor-content :editor="editor" v-if="editor" />
+</template>
+
+<script lang="ts">
+import { Editor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+import { defineComponent } from '@vue/runtime-core'
+
+export default defineComponent({
+  components: {
+    EditorContent,
+  },
+
+  data() {
+    return {
+      editor: null,
+    } as { editor: Editor | null }
+  },
+
+  mounted() {
+    this.editor = new Editor({
+      content: '<p>I’m running tiptap with Vue.js. 🎉</p>',
+      extensions: [
+        StarterKit,
+      ],
+    })
+  },
+
+  beforeUnmount() {
+    this.editor?.destroy()
+  },
+});
+</script>
